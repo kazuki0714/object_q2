ee3f13e328311974b844e3bb5a6d8bff5c745039
Kazuki_Ouchi <kazukiouchi0714@gmail.com>
2019-07-29 19:40:04 +0900

    破線をcar.rbから移した

--- main.rb
+++ main.rb
@@ -5,12 +5,19 @@ require_relative 'nissan'
 
 honda = Honda.new
 honda.output_information(honda)
+ puts '------------------------------'
 
 nissan = Nissan.new
 nissan.output_information(nissan)
+ puts '------------------------------'
 
 ferrari = Ferrari.new
 ferrari.output_information(ferrari)
 
+puts '-----フェラーリ（リフトアップ）----'
 ferrari.lift_up(ferrari)
+ferrari.output_information(ferrari)
+
+puts '-----フェラーリ（リフトダウン）----'
 ferrari.lift_down(ferrari)
+ferrari.output_information(ferrari)
